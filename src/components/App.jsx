ae8db0885d3fbd1c67e8b014d315b7c47bbff285
Yuliia Cherkashyna <jul11091109@gmail.com>
2022-12-27 23:38:35 +0200

    зміни

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,29 @@ import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
 import css from './App.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/contactSelectors';
+
 
 export function App () {
+
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
     return (
       <div>
       <h1 className={css.text}>Phonebook</h1>
       <ContactForm  />
       <h2  className={css.text}>Contacts</h2>
       <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
       <ContactList />
       </div>
     );
